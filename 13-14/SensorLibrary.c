88d048f93765967c36e21b51d29e67aa27ab1dc7
Chuck-robotics <charles@herridge.org>
2013-09-03 18:36:31 -0500

    Function Libraries
    IR added

--- 13-14/SensorLibrary.c
+++ 13-14/SensorLibrary.c
@@ -12,3 +12,14 @@ float getDistanceUS()
 {
 
 }
+
+
+/*-------------------------------------------------------------IR-Sensor----------------------------------------------------------------------------------------*/
+
+if
+
+function moveUntil(string sensorLeft,string sensorRight,string motorLeft,string motorRight,int thresholdLeft,int thresholdRight)
+{
+
+
+}
---
+++ Libraries/IR_library.c
@@ -0,0 +1,9 @@
+/*-------------------------------------------------------------IR-Sensor----------------------------------------------------------------------------------------*/
+
+function valueDisplay
+
+function moveUntil(string sensorLeft,string sensorRight,string motorLeft,string motorRight,int thresholdLeft,int thresholdRight, int mpowerLeft, int mpowerRight)
+{
+
+
+}
